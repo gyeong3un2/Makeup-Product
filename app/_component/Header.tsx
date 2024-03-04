c5f1418d4c9bfdff839ca0a6bd5d8de38461e6d5
박경은 <ge.park@danbiedu.co.kr>
2024-03-04 11:25:48 +0900

    feature: Header Component 추가

---
+++ app/_component/Header.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+function Header() {
+  return(
+    <Box className='text-center p-12 text-[43px]' >
+      Listing Products
+    </Box>
+  )
+}
+
+export default Header;
