eddbc78c1d369ba7220e078452f739d3478bf60f
박경은 <ge.park@danbiedu.co.kr>
2024-03-07 09:29:09 +0900

    feature: Skeleton UI 추가

--- src/app/_components/DisplayProducts.tsx
+++ src/app/_components/DisplayProducts.tsx
@@ -5,6 +5,8 @@ import ProductInfo from '../../components/ui/ProductInfo';
 import { useGetAllProductList } from '@/src/api/product';
 import { useEffect } from 'react';
 import { GetProductListResponse } from '@/src/types/index';
+import SkeletonUI from './SkeletonUI';
+import { numbers } from '@/src/data/dummy';
 
 function DisplayProducts() {
   const { status, data: productList } = useGetAllProductList();
@@ -15,26 +17,31 @@ function DisplayProducts() {
 
   return (
     <Grid container rowSpacing={2}>
-      {productList &&
-        productList.map((product: GetProductListResponse) => (
-          <Grid item xs={12} sm={3} md={3} key={product.id}>
-            <Card className="shadow-none max-screen-w-64 hover:cursor-pointer hover:shadow-2xl transition ease-in-out hover:-translate-y-1 duration-300">
-              <Image
-                className="w-36 m-auto my-3"
-                src={`http:${product.api_featured_image}`}
-                alt={product.name}
-                width={24}
-                height={24}
-              />
+      {productList
+        ? productList.map((product: GetProductListResponse) => (
+            <Grid item xs={12} sm={3} md={3} key={product.id}>
+              <Card className="shadow-none max-screen-w-64 hover:cursor-pointer hover:shadow-2xl transition ease-in-out hover:-translate-y-1 duration-300">
+                <Image
+                  className="w-36 m-auto my-3"
+                  src={`http:${product.api_featured_image}`}
+                  alt={product.name}
+                  width={24}
+                  height={24}
+                />
 
-              <CardContent>
-                <ProductInfo />
+                <CardContent>
+                  <ProductInfo />
 
-                <ProductColor />
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+                  <ProductColor />
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        : numbers.map((number) => (
+            <Grid item xs={12} sm={3} md={3} key={number}>
+              <SkeletonUI />
+            </Grid>
+          ))}
     </Grid>
   );
 }
