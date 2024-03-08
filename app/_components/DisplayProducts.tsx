ec5559806f7794ceef1cebc21b87f3bf07a0668f
박경은 <ge.park@danbiedu.co.kr>
2024-03-08 15:24:40 +0900

    feature: 상품 리스트 반응형 디자인 구현

--- app/_components/DisplayProducts.tsx
+++ app/_components/DisplayProducts.tsx
@@ -14,7 +14,7 @@ function DisplayProducts({ productList, fetchStatus }: IProductListProps) {
     <Grid container rowSpacing={2} gridAutoRows={1}>
       {productList && fetchStatus === 'idle'
         ? productList.map((product: GetProductListResponse) => (
-            <Grid item xs={12} sm={3} md={3} key={product.id}>
+            <Grid item xs={12} sm={6} md={3} key={product.id}>
               <Card className="h-full shadow-none max-screen-w-64 hover:cursor-pointer hover:shadow-2xl transition ease-in-out hover:-translate-y-1 duration-300">
                 <Image
                   className="w-36 m-auto my-3"
@@ -33,7 +33,7 @@ function DisplayProducts({ productList, fetchStatus }: IProductListProps) {
             </Grid>
           ))
         : skeletonCounts.map((count) => (
-            <Grid item xs={12} sm={3} md={3} key={count}>
+            <Grid item xs={12} sm={6} md={3} key={count}>
               <SkeletonUI />
             </Grid>
           ))}
